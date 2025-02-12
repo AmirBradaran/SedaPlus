d8f2c4e8cadae73dc51ec8c19b257ab9175bd6cb
Amir <baradaran13085@gmail.com>
2025-02-12 17:33:21 +0330

    Update index.jsx

--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -2,7 +2,8 @@ import SliderHm from "./SliderHm/index";
 import CategoriesHm from "./CategoriesHm/index";
 import BestSells from "./BestSells/index";
 import NewstSells from "./NewstSells/index";
-import SuggestionSlider from "./SuggestionSlider/index";
+import SuggestionLeftSlider from "./SuggestionLeftSlider/index";
+import SuggestionRightSlider from "./SuggestionRightSlider/index";
 import React from "react";
 import { Stack } from "@mui/material";
 import SuggestionCarts from "./SuggestionCarts";
@@ -14,15 +15,15 @@ export default function Home() {
         sx={{
           display: "flex",
           flexDirection: "row",
+          justifyContent:"center",
           flexWrap:"wrap",
-          px:12,
           py:5,
           background:"linear-gradient(to bottom, var(--forth-color), var(--white-color))"
         }}
       >
-        <SuggestionSlider />
+        <SuggestionLeftSlider />
         <SliderHm />
-        <SuggestionSlider />
+        <SuggestionRightSlider />
       </Stack>
       <CategoriesHm />
       <SuggestionCarts/>/
