65395e446b5c8ac440f7f1b0e2cbbfa17a418afd
Amir <baradaran13085@gmail.com>
2025-02-14 00:38:14 +0330

    Update index.jsx

--- front-end/src/Pages/NotFound/index.jsx
+++ front-end/src/Pages/NotFound/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Box, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export default function NotFound() {
+const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      textAlign="center"
+    >
+      <Typography variant="h1" color="primary" gutterBottom>
+       404
+      </Typography>
+      <Typography variant="h5" color="textSecondary" gutterBottom>
+        اوه! صفحه‌ای که به دنبال آن هستید وجود ندارد.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{background:"var(--seven-color)" , fontSize:"1.25rem"}}
+        onClick={() => navigate("/")}
+      >
+        بازگشت به صفحه اصلی
+      </Button>
+    </Box>
+  );
+};
+
+export default NotFound;
