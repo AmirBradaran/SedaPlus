db816d998333560d4f02778cd30d7ba04d7dfb59
Amir <baradaran13085@gmail.com>
2025-04-03 04:37:33 +0330

    Update AuthSlice.js

--- front-end/src/Store/Slices/AuthSlice.js
+++ front-end/src/Store/Slices/AuthSlice.js
@@ -1,20 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
-  token: null,
+  token: localStorage.getItem("token") || null,
   user: null,
 };
+
 const authSlice = createSlice({
-  name: "authSlice",
+  name: "auth",
   initialState,
   reducers: {
     login: (state, action) => {
       state.token = action.payload.token;
       state.user = action.payload.user;
+      localStorage.setItem("token", action.payload.token);
     },
     logout: (state) => {
-      (state.token = null), (state.user = null);
+      state.token = null;
+      state.user = null;
+      localStorage.removeItem("token"); 
     },
   },
 });
-export const{login , logout}=authSlice.actions
-export default authSlice.reducer
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
