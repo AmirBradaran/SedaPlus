5c260d4a6522327852e80d20288bc97e5ea8f818
Amir <baradaran13085@gmail.com>
2025-04-06 00:47:19 +0330

    Update index.jsx

--- front-end/src/Components/Navbar/index.jsx
+++ front-end/src/Components/Navbar/index.jsx
@@ -2,284 +2,244 @@ import React, { useState, useMemo } from "react";
 import {
   Badge,
   Box,
-  Button,
   IconButton,
-  Menu,
-  MenuItem,
   Typography,
   useMediaQuery,
-  useTheme,
-  SwipeableDrawer,
-  List,
-  ListItem,
-  Divider,
-  styled,
+  BottomNavigation,
+  BottomNavigationAction,
+  Paper,
   Stack,
 } from "@mui/material";
 import {
-  DarkMode,
   CardGiftcard,
   FavoriteBorder,
   Search,
-  Menu as MenuIcon,
-  Close,
   Shuffle,
+  Home,
+  Person,
+  Info,
+  Article,
+  Login,
 } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const StyledLink = styled(Link)(({ theme }) => ({
-  textDecoration: "none",
-  color: "var(--third-color)",
-  transition: "all 0.15s ease",
-  fontFamily: "IranYekan, sans-serif",
-  "&:hover": {
-    color: "var(--first-color)",
-    transform: "translateY(-2px)",
-  },
-}));
+{
+  /*Styles*/
+}
+const iconStyle = {
+  fontSize: 35,
+  color: "common.white",
+  transition: "all 0.3s",
+  ":hover": { transform: "translateY(-10px)" },
+};
 
+const typographyStyle = {
+  fontFamily: "IranYekan",
+  fontSize: "1.5rem",
+  fontWeight: "bold",
+  display: "flex",
+  alignItems: "center",
+  gap: 1,
+  color: "var(--third-color)",
+  transition: "all 0.3s",
+  ":hover": {
+    color: "#b8b8b8",
+    transform: "translateY(-5px)",
+  },
+};
+
+const navActionStyle = {
+  color: "white",
+  transition: "all 0.3s",
+  ":hover": { transform: "translateY(-5px)" },
+  ".MuiBottomNavigationAction-label": {
+    fontFamily: "IranYekan",
+    fontSize: "1rem",
+    fontWeight: "bold",
+  },
+};
+
+{
+  /*Component Styles*/
+}
+const StyledLink = ({ to, children }) => (
+  <Link
+    to={to}
+    style={{
+      textDecoration: "none",
+      color: "var(--third-color)",
+      fontFamily: "IranYekan, sans-serif",
+      transition: "all 0.3s",
+    }}
+  >
+    {children}
+  </Link>
+);
+
+{
+  /*NavBar Components*/
+}
 const Navbar = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  const [menuAnchor, setMenuAnchor] = useState(null);
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width:1244px)");
+  const [bottomNavValue, setBottomNavValue] = useState(0);
 
   const cartLength = useSelector((state) => state.cart?.items?.length) || 0;
   const token = useSelector((state) => true);
 
   const navItems = useMemo(
     () => [
-      { label: "درباره ما", path: "/about-us" },
-      { label: token ? "پروفایل" : "ورود", path: token ? "/profile" : "/auth" },
-      { label: "مجله ها", path: "/magazine" },
-      { label: "خانه", path: "/" },
+      {
+        label: token ? "پروفایل" : "ورود",
+        path: token ? "/profile" : "/auth",
+        icon: token ? <Person /> : <Login />,
+      },
+      { label: "مجله ها", path: "/magazine", icon: <Article /> },
+      { label: "درباره ما", path: "/about-us", icon: <Info /> },
+      { label: "خانه", path: "/", icon: <Home /> },
     ],
     [token]
   );
 
-  const categories = ["لوازم جانبی", "هدفون", "هدست", "هندزفری"];
+  {
+    /*Render ke help mikone*/
+  }
+  const renderIconLink = (to, icon, withBadge = false) => (
+    <StyledLink to={to}>
+      <IconButton>
+        {withBadge ? (
+          <Badge badgeContent={cartLength} color="secondary">
+            {icon}
+          </Badge>
+        ) : (
+          icon
+        )}
+      </IconButton>
+    </StyledLink>
+  );
 
   return (
-    <Stack
-      component="nav"
-      mx="auto"
-      sx={{
-        position: "relative",
-        width: { xs: "100%", sm: "95%", md: "90%" },
-        height: "100%",
-        zIndex: 1200,
-        background:
-          "linear-gradient(to right , var(--second-color) 65%, #7A4A94)",
-        py: 2,
-        borderBottomRightRadius: {
-          xs: "40px",
-          sm: "50px",
-          md: "60px",
-          lg: "80px",
-        },
-        borderBottomLeftRadius: {
-          xs: "40px",
-          sm: "50px",
-          md: "60px",
-          lg: "80px",
-        },
-      }}
-    >
+    <>
       <Stack
-        justifyContent="space-between"
-        alignItems="center"
-        direction={"row"}
+        component="nav"
+        mx="auto"
+        direction="row"
         sx={{
-          mx: { xs: 1, md: 5 },
-          px: { xs: 0, md: 4 },
+          position: "relative",
+          width: "95%",
+          height: "100px",
+          zIndex: 1200,
+          background:
+            "linear-gradient(to right , var(--second-color) 65%, #7A4A94)",
+          py: 2,
+          borderBottomRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Stack direction="row" sx={{ gap: { xs: 0, sm: 1 } }}>
-          <StyledLink to="/cart">
-            <IconButton>
-              <Badge badgeContent={cartLength} color="secondary">
-                <CardGiftcard sx={{ fontSize: 32, color: "common.white" }} />
-              </Badge>
-            </IconButton>
-          </StyledLink>
-
-          <StyledLink to="/favorites">
-            <IconButton>
-              <FavoriteBorder sx={{ fontSize: 32, color: "common.white" }} />
-            </IconButton>
-          </StyledLink>
-
-          <StyledLink to="/search">
-            <IconButton>
-              <Search sx={{ fontSize: 32, color: "common.white" }} />
-            </IconButton>
-          </StyledLink>
-          <StyledLink to="/shuffle">
-            <IconButton>
-              <Shuffle sx={{ fontSize: 32, color: "common.white" }} />
-            </IconButton>
-          </StyledLink>
-        </Stack>
-
-        <Stack>
+        {/* Left Icons */}
+        <Stack direction="row" sx={{ gap: 1, position: "absolute", left: 15 }}>
+          {renderIconLink("/cart", <CardGiftcard sx={iconStyle} />, true)}
           {isMobile ? (
-            <IconButton
-              onClick={() => setDrawerOpen(true)}
-              sx={{ position: "absolute", right: 15, top: 15 }}
-            >
-              <MenuIcon fontSize="large" sx={{ color: "var(--third-color)" }} />
-            </IconButton>
+            renderIconLink("/search", <Search sx={iconStyle} />)
           ) : (
-            <Stack gap={5} direction="row">
-              {navItems.map((item) =>
-                item.type === "menu" ? (
-                  <Button
-                    sx={{
-                      color: "var(--third-color)",
-                      fontSize: "1.25rem",
-                      fontWeight: "bold",
-                      fontFamily: "IranYekan",
-                      p: 0,
-                      transition: "all 0.25s",
-                      ":hover": {
-                        color: "#b8b8b8",
-                        translate: "0 -15%",
-                      },
-                    }}
-                    key={item.label}
-                    onMouseEnter={(e) => setMenuAnchor(e.currentTarget)}
-                  >
-                    {item.label}
-                  </Button>
-                ) : (
-                  <StyledLink key={item.label} to={item.path}>
-                    <Typography
-                      sx={{
-                        fontSize: "1.25rem",
-                        color: "var(--third-color)",
-                        fontWeight: "bold",
-                        fontFamily: "IranYekan",
-                        transition: "all 0.35s",
-                        ":hover": { color: "#b8b8b8" },
-                      }}
-                      
-                    >
-                      {item.label}
-                    </Typography>
-                    
-                  </StyledLink>
-                  
-                )
-              )}
-            </Stack>
+            <>
+              {renderIconLink("/favorites", <FavoriteBorder sx={iconStyle} />)}
+              {renderIconLink("/search", <Search sx={iconStyle} />)}
+              {renderIconLink("/shuffle", <Shuffle sx={iconStyle} />)}
+            </>
           )}
         </Stack>
 
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent={{ xs: "center", md: "flex-start" }}
-          sx={{ width: { xs: "100%", md: "auto" }, position: "relative" }}
-        >
-          <StyledLink to="/">
-            <Box
-              component="img"
-              src="/DigiSeda.png"
-              alt="Logo"
-              sx={{
-                borderRadius: "50%",
-                width: 70,
-                height: 70,
-                objectFit: "cover",
-                position: { xs: "absolute", md: "static" },
-                left: { xs: "25%", md: "auto" },
-                transform: { xs: "translate(-50%)", md: "none" },
-                top: -35,
-              }}
-            />
-          </StyledLink>
-        </Stack>
-      </Stack>
-
-      {/* دسته بندی ها */}
-      <Menu
-        anchorEl={menuAnchor}
-        open={Boolean(menuAnchor)}
-        onClose={() => setMenuAnchor(null)}
-      >
-        {categories.map((category) => (
-          <MenuItem
+        {/* Logo */}
+        <StyledLink to="/">
+          <Box
+            component="img"
+            src="/DigiSeda.png"
+            alt="Logo"
             sx={{
-              width: "150px",
-              transition: "all 0.15s",
-              ":hover": {
-                background:
-                  "linear-gradient(135deg , var(--second-color) 65%, white)",
-                color: "var(--third-color)",
-              },
-              fontFamily: "IranYekan",
+              borderRadius: "50%",
+              width: 90,
+              height: 90,
+              objectFit: "cover",
+              position: "absolute",
+              left: "50%",
+              top: 5,
+              transform: "translateX(-50%)",
             }}
-            key={category}
-            onClick={() => setMenuAnchor(null)}
-          >
-            {category}
-          </MenuItem>
-        ))}
-      </Menu>
+          />
+        </StyledLink>
 
-      {/* منو موبایل */}
-      <SwipeableDrawer
-        anchor="right"
-        open={drawerOpen}
-        onClose={() => setDrawerOpen(false)}
-      >
-        <Box
-          sx={{
-            width: 280,
-            height: "100%",
-            p: 2,
-            background:
-              "linear-gradient(135deg, var(--second-color), var(--third-color))",
-            color: "white",
-          }}
-        >
-          <IconButton
-            onClick={() => setDrawerOpen(false)}
-            sx={{ position: "absolute ", right: "5px" }}
+        {/* Right Icons */}
+        {isMobile && (
+          <Stack
+            direction="row"
+            sx={{ gap: 1, position: "absolute", right: 15 }}
           >
-            <Close />
-          </IconButton>
-          <List>
-            {navItems.map((item) => (
-              <ListItem key={item.label}>
-                <StyledLink to={item.path}>
-                  <Typography sx={{ fontFamily: "IranYekan" }}>
-                    {item.label}
-                  </Typography>
-                </StyledLink>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            <Typography
-              variant="h6"
-              sx={{ px: 2, py: 1.5, fontFamily: "IranYekan" }}
-            >
-              دسته بندی ها
-            </Typography>
-            {categories.map((category) => (
-              <ListItem key={category}>
-                <Typography sx={{ fontFamily: "IranYekan" }}>
-                  {category}
+            {renderIconLink("/favorites", <FavoriteBorder sx={iconStyle} />)}
+            {renderIconLink("/shuffle", <Shuffle sx={iconStyle} />)}
+          </Stack>
+        )}
+
+        {/* Desktop Navigation */}
+        {!isMobile && (
+          <Stack gap={5} direction="row" position="absolute" right={15}>
+            {navItems.map(({ label, path, icon }) => (
+              <StyledLink key={label} to={path}>
+                <Typography sx={typographyStyle}>
+                  {icon}
+                  {label}
                 </Typography>
-              </ListItem>
+              </StyledLink>
             ))}
-          </List>
-        </Box>
-      </SwipeableDrawer>
-    </Stack>
+          </Stack>
+        )}
+
+        {/* Mobile Bottom Navigation */}
+        {isMobile && (
+          <Paper
+            sx={{
+              position: "fixed",
+              bottom: 0,
+              left: 0,
+              right: 0,
+              zIndex: 1200,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              background:
+                "linear-gradient(to right , var(--second-color) 65%, #7A4A94)",
+              width: "90%",
+              mx: "auto",
+            }}
+            elevation={8}
+          >
+            <BottomNavigation
+              showLabels
+              value={bottomNavValue}
+              onChange={(e, newValue) => setBottomNavValue(newValue)}
+              sx={{
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+                background:
+                  "linear-gradient(to right , var(--second-color) 65%, #7A4A94)",
+              }}
+            >
+              {navItems.map(({ label, path, icon }) => (
+                <BottomNavigationAction
+                  key={label}
+                  component={Link}
+                  to={path}
+                  label={label}
+                  icon={icon}
+                  sx={navActionStyle}
+                />
+              ))}
+            </BottomNavigation>
+          </Paper>
+        )}
+      </Stack>
+    </>
   );
 };
 
